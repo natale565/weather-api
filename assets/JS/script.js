51ba33bd71495d772447177eb9832a1d91392e30
Chris Natale <natale565@icloud.com>
2024-07-09 20:04:50 -0400

    fixed JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,11 @@
-var key = "6ea439e2ac8153eaa031324315bedfd3";
-var city = "Windsor"
+ let key = "6ea439e2ac8153eaa031324315bedfd3";
+let city = "Windsor"
 
 //Grabs the current time and date
-var date = moment().format('dddd, MMMM Do YYYY');
-var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
+let date = moment().format('dddd, MMMM Do YYYY');
+let dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 
-var cityHist = [];
+let cityHist = [];
 //Will save the text value of the search and save it to an array and storage
 $('.search').on("click", function (event) {
 	event.preventDefault();
@@ -22,14 +22,14 @@ $('.search').on("click", function (event) {
 });
 
 //Will create buttons based on search history 
-var contHistEl = $('.cityHist');
+let contHistEl = $('.cityHist');
 function getHistory() {
 	contHistEl.empty();
 
 	for (let i = 0; i < cityHist.length; i++) {
 
-		var rowEl = $('<row>');
-		var btnEl = $('<button>').text(`${cityHist[i]}`)
+		let rowEl = $('<row>');
+		let btnEl = $('<button>').text(`${cityHist[i]}`)
 
 		rowEl.addClass('row histBtnRow');
 		btnEl.addClass('btn btn-outline-secondary histBtn');
@@ -50,10 +50,10 @@ function getHistory() {
 };
 
 //Grab the main 'Today' card body.
-var cardTodayBody = $('.cardBodyToday')
+let cardTodayBody = $('.cardBodyToday')
 //Applies the weather data to the today card and then launches the five day forecast
 function getWeatherToday() {
-	var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
+	let getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
 
 	$(cardTodayBody).empty();
 
@@ -66,62 +66,41 @@ function getWeatherToday() {
 		//Icons
 		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
 		// Temperature
-		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
+		let pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
 		cardTodayBody.append(pEl);
 		//Feels Like
-		var pElTemp = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
+		let pElTemp = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
 		cardTodayBody.append(pElTemp);
 		//Humidity
-		var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
+		let pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
 		cardTodayBody.append(pElHumid);
 		//Wind Speed
-		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
+		let pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
 		cardTodayBody.append(pElWind);
 		//Set the lat and long from the searched city
-		var cityLon = response.coord.lon;
+		let cityLon = response.coord.lon;
 		// console.log(cityLon);
-		var cityLat = response.coord.lat;
+		let cityLat = response.coord.lat;
 		// console.log(cityLat);
 
-		var getUrlUvi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${key}`;
+		
 
-		$.ajax({
-			url: getUrlUvi,
-			method: 'GET',
-		}).then(function (response) {
-			var pElUvi = $('<p>').text(`UV Index: `);
-			var uviSpan = $('<span>').text(response.current.uvi);
-			var uvi = response.current.uvi;
-			pElUvi.append(uviSpan);
-			cardTodayBody.append(pElUvi);
-			//set the UV index to match an exposure chart severity based on color 
-			if (uvi >= 0 && uvi <= 2) {
-				uviSpan.attr('class', 'green');
-			} else if (uvi > 2 && uvi <= 5) {
-				uviSpan.attr("class", "yellow")
-			} else if (uvi > 5 && uvi <= 7) {
-				uviSpan.attr("class", "orange")
-			} else if (uvi > 7 && uvi <= 10) {
-				uviSpan.attr("class", "red")
-			} else {
-				uviSpan.attr("class", "purple")
-			}
-		});
+		
 	});
 	getFiveDayForecast();
 };
 
-var fiveForecastEl = $('.fiveForecast');
+let fiveForecastEl = $('.fiveForecast');
 
 function getFiveDayForecast() {
-	var getUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
+	let getUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
 
 	$.ajax({
 		url: getUrlFiveDay,
 		method: 'GET',
 	}).then(function (response) {
-		var fiveDayArray = response.list;
-		var myWeather = [];
+		let fiveDayArray = response.list;
+		let myWeather = [];
 		//Made a object that would allow for easier data read
 		$.each(fiveDayArray, function (index, value) {
 			testObj = {
@@ -140,34 +119,34 @@ function getFiveDayForecast() {
 		//Inject the cards to the screen 
 		for (let i = 0; i < myWeather.length; i++) {
 
-			var divElCard = $('<div>');
+			let divElCard = $('<div>');
 			divElCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
 			divElCard.attr('style', 'max-width: 200px;');
 			fiveForecastEl.append(divElCard);
 
-			var divElHeader = $('<div>');
+			let divElHeader = $('<div>');
 			divElHeader.attr('class', 'card-header')
-			var m = moment(`${myWeather[i].date}`).format('MM-DD-YYYY');
+			let m = moment(`${myWeather[i].date}`).format('MM-DD-YYYY');
 			divElHeader.text(m);
 			divElCard.append(divElHeader)
 
-			var divElBody = $('<div>');
+			let divElBody = $('<div>');
 			divElBody.attr('class', 'card-body');
 			divElCard.append(divElBody);
 
-			var divElIcon = $('<img>');
+			let divElIcon = $('<img>');
 			divElIcon.attr('class', 'icons');
 			divElIcon.attr('src', `https://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
 			divElBody.append(divElIcon);
 
 			//Temp
-			var pElTemp = $('<p>').text(`Temperature: ${myWeather[i].temp} °F`);
+			let pElTemp = $('<p>').text(`Temperature: ${myWeather[i].temp} °F`);
 			divElBody.append(pElTemp);
 			//Feels Like
-			var pElFeel = $('<p>').text(`Feels Like: ${myWeather[i].feels_like} °F`);
+			let pElFeel = $('<p>').text(`Feels Like: ${myWeather[i].feels_like} °F`);
 			divElBody.append(pElFeel);
 			//Humidity
-			var pElHumid = $('<p>').text(`Humidity: ${myWeather[i].humidity} %`);
+			let pElHumid = $('<p>').text(`Humidity: ${myWeather[i].humidity} %`);
 			divElBody.append(pElHumid);
 		}
 	});
@@ -176,7 +155,7 @@ function getFiveDayForecast() {
 //Allows for the example data to load for Windsor
 function initLoad() {
 
-	var cityHistStore = JSON.parse(localStorage.getItem('city'));
+	let cityHistStore = JSON.parse(localStorage.getItem('city'));
 
 	if (cityHistStore !== null) {
 		cityHist = cityHistStore
